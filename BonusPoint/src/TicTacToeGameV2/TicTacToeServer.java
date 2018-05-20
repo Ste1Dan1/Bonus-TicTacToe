bf2ae3b91cb1bebb56e63325f7798acd9c426241
Dani <Dani@Dani-PC>
2018-05-20 21:07:29 +0200

    server

---
+++ BonusPoint/src/TicTacToeGameV2/TicTacToeServer.java
@@ -0,0 +1,59 @@
+package TicTacToeGameV2;
+
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import java.util.logging.Logger;
+
+public class TicTacToeServer {
+
+	private final Logger logger = Logger.getLogger("");
+	int port;
+
+	// Have the server run until it is killed.
+	private ServerSocket listener;
+	private volatile boolean stop = false;
+
+	public void startServer(int port) {
+		logger.info("Start server");
+		try {
+			listener = new ServerSocket(port, 10, null);
+			Runnable r = new Runnable() {
+				@Override
+				public void run() {
+					while (!stop) {
+						try {
+							Socket socket = listener.accept();
+							// TODO
+
+						} catch (Exception e) {
+							logger.info(e.toString());
+						}
+					}
+				}
+			};
+			Thread t = new Thread(r, "ServerSocket");
+			t.start();
+		} catch (IOException e) {
+			logger.info(e.toString());
+		}
+	}
+
+	public void stopServer() {
+		logger.info("Stop all clients");
+
+		logger.info("Stop server");
+		stop = true;
+		if (listener != null) {
+			try {
+				listener.close();
+			} catch (IOException e) {
+				// Uninteresting
+			}
+		}
+	}
+}
